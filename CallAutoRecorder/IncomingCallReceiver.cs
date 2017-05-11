f37abae5448199ae45f8f73dd0bf3c92433950e2
Takahito Iwasa <iwasa-t@pise.co.jp>
2017-05-11 21:05:13 +0900

    Add project files.

---
+++ CallAutoRecorder/IncomingCallReceiver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Android.Telephony;
+
+namespace CallAutoRecorder
+{
+    [BroadcastReceiver]
+    [IntentFilter(new[] { "android.intent.action.PHONE_STATE" })]
+    public class IncomingCallReceiver : BroadcastReceiver
+    {
+        public TelephonyManager _Manager { get; set; }
+        public override void OnReceive(Context context, Intent intent)
+        {
+            _Manager = context.GetSystemService(Context.TelephonyService) as TelephonyManager;
+            if (_Manager == null)
+                return;
+
+            var listener = new PhoneStateListenerEx(context);
+            _Manager.Listen(listener, PhoneStateListenerFlags.CallState);
+        }
+
+        private class PhoneStateListenerEx : PhoneStateListener
+        {
+            private Context _Context { get; set; }
+            private VoiceCallRecorder _Recorder { get; set; }
+            public PhoneStateListenerEx(Context context)
+            {
+                _Context = context;
+
+                try
+                {
+                    _Recorder = new VoiceCallRecorder();
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(context, ex.ToString(), ToastLength.Long).Show();
+                }
+
+                _Recorder.RecFinished += (sender, e) =>
+                {
+                    //TODO:録音終了
+                    Toast.MakeText(_Context, e.IncomingNumber, ToastLength.Short).Show();
+                    Toast.MakeText(_Context, $"音声ファイル保存先：{e.RecAudioPath}", ToastLength.Short).Show();
+                };
+            }
+
+            public override void OnCallStateChanged([GeneratedEnum] CallState state, string incomingNumber)
+            {
+                if(state == CallState.Offhook)
+                {
+                    if (_Recorder != null)
+                    {
+                        _Recorder.RecStart();
+                    }
+                }
+                else if (state == CallState.Idle)
+                {
+                    if (_Recorder != null)
+                    {
+                        _Recorder.RecStop(incomingNumber);
+                        _Recorder.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CallAutoRecorder/VoiceCallRecorder.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+using Java.IO;
+
+namespace CallAutoRecorder
+{
+    class VoiceCallRecorder : IDisposable
+    {
+        public MediaRecorder _Recorder { get; set; }
+
+        public bool IsRec { get; private set; }
+
+        public event EventHandler<RecFinishedArgs> RecFinished;
+
+        private string _OutputPath { get; set; }
+
+        public VoiceCallRecorder()
+        {
+            //https://developer.android.com/reference/android/media/MediaRecorder.html
+            _Recorder = new MediaRecorder();
+
+            _Recorder.SetAudioSource(AudioSource.VoiceCall);
+            //API requirements
+            //Container : WAV
+            //Encoding : PCM
+            //Rate : 16K
+            //Sample Format : 16bit
+            //Channels : Mono
+
+            //AndroidはWAV対応してないようなので自前で組み込む必要があるとのこと
+            //http://stackoverflow.com/questions/4871149/how-to-record-voice-in-wav-format-in-android
+
+            _Recorder.SetOutputFormat(OutputFormat.Mpeg4);
+            _Recorder.SetAudioEncoder(AudioEncoder.Aac);
+
+            var storage = File.CreateTempFile("hoge_", ".mp4", Android.OS.Environment.ExternalStorageDirectory);
+            _OutputPath = storage.AbsolutePath;
+
+            _Recorder.SetOutputFile(_OutputPath);
+            _Recorder.Prepare();
+        }
+        public void RecStart()
+        {
+            if (IsRec) return;
+
+            _Recorder.Start();
+            IsRec = true;
+        }
+
+        public void RecStop(string number)
+        {
+            if (!IsRec) return;
+
+            _Recorder.Stop();
+            IsRec = false;
+            
+            RecFinished?.Invoke(this, new RecFinishedArgs
+            {
+                RecAudioPath = _OutputPath,
+                IncomingNumber = number,
+            });
+        }
+
+        public void Dispose()
+        {
+            if(_Recorder != null)
+            {
+                _Recorder.Release();
+                _Recorder = null;
+            }
+        }
+    }
+
+    public class RecFinishedArgs : EventArgs
+    {
+        public string RecAudioPath { get; set; }
+
+        public string IncomingNumber { get; set; }
+    }
+}
